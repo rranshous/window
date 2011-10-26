fa40c114361fe53252a27ba8fd29a1316af655e4
Robby Ranshous <rranshous@gmail.com>
2011-10-25 21:52:17 -0400

    fix in loopable to removing refs, udp input renamed

--- loopable.py
+++ loopable.py
@@ -10,21 +10,33 @@ class Loopable(object):
     def loop_all(cls,count=None):
         """ loop forever if no count """
         print 'looping all'
+        # do we wanna loop a few times?
         if count:
             for i in xrange(count):
                 cls._loop_all()
+
+        # or all the time?
         else:
             while True:
                 cls._loop_all()
 
     @classmethod
     def _loop_all(cls):
+        to_remove = []
+        # go through all the instances
         for inst_ref in cls.instances:
             instance = inst_ref()
+            # if the ref is to something which still exists
+            # call it's loop
             if instance is not None:
                 instance.loop()
             else:
-                cls.instances.remove(inst_ref)
+                # if it doesnt exist mark it for removal
+                to_remove.append(inst_ref)
+
+        # get rid of dead refs
+        for inst_ref in to_remove:
+            cls.instances.remove(inst_ref)
 
     # child should over ride this and do their loop work
     def loop(self):
